8fa0cd0623be09660d3f1ee9f29b8a04d95450ac
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-09-05 22:30:56 -0600

    cubit example

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bloc Counter'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh_outlined),
+            onPressed: () => {},
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Counter value: xxxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 1,
+            child: const Text('+3'),
+            onPressed: () => {},
+          ),
+
+          const SizedBox(height: 15,),
+
+          FloatingActionButton(
+            heroTag: 2,
+            child: const Text('+2'),
+            onPressed: () => {},
+          ),
+
+          const SizedBox(height: 15,),
+
+          FloatingActionButton(
+            heroTag: 3,
+            child: const Text('+1'),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+    );
+  }
+}
